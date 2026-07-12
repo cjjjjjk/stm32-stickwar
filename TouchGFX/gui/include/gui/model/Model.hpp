33ff56f4629ca8070d49f910626685e00e9326fb
TranHuy <emhuy7a@gmail.com>
2026-07-12 18:14:24 +0700

    Model

--- TouchGFX/gui/include/gui/model/Model.hpp
+++ TouchGFX/gui/include/gui/model/Model.hpp
@@ -14,8 +14,27 @@ public:
     }
 
     void tick();
+
+    // --- THÊM CÁC HÀM QUẢN LÝ BEST OF 3 Ở ĐÂY ---
+    void increaseWinA();
+    void increaseWinB();
+    int getWinsA() const { return winsA; }
+    int getWinsB() const { return winsB; }
+
+    void nextRound();
+    int getCurrentRound() const { return currentRound; }
+
+    void resetMatchData();
+    // --------------------------------------------
+
 protected:
     ModelListener* modelListener;
+
+    // --- THÊM CÁC BIẾN TRẠNG THÁI Ở ĐÂY ---
+    int winsA;
+    int winsB;
+    int currentRound;
+    // --------------------------------------
 };
 
 #endif // MODEL_HPP

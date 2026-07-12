93d6e7d3d13bd7d66143c53340ca3757e33b86c6
TranHuy <emhuy7a@gmail.com>
2026-07-12 20:15:47 +0700

    fix: restore full structure for app.mk

--- TouchGFX/config/gcc/app.mk
+++ TouchGFX/config/gcc/app.mk
@@ -1,8 +1,20 @@
-
 # Relative location of the TouchGFX framework from root of application
 touchgfx_path := ../Middlewares/ST/touchgfx
 
 # Location of the TouchGFX Environment
 touchgfx_env := ../../../../TouchGFX/4.26.1/env
+
 # Optional additional compiler flags
 user_cflags := -DUSE_BPP=16
+
+# Any Additional components
+# e.g. components := 
+
+# Any Additional include paths
+# e.g. include_paths := 
+
+# Additional compiler/linker flags
+# e.g. board_cflags :=
+#      board_lflags :=
+
+include $(touchgfx_path)/config/gcc/app.mk
